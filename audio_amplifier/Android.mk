1eea2548c932ba80de3fbb83098a132cc1178201
Yuan Si <do4suki@gmail.com>
2022-08-24 22:55:11 +0800

    treewide: Clean up for AOSPA buiding
    Signed-off-by: Yuan Si <do4suki@gmail.com>
    Change-Id: Id3342a7a8c50d5b40d8780ce3755cd3fb1394e80

--- audio_amplifier/Android.mk
+++ audio_amplifier/Android.mk
@@ -34,7 +34,7 @@ LOCAL_C_INCLUDES += \
     vendor/qcom/opensource/audio-hal/primary-hal/hal
 
 LOCAL_HEADER_LIBRARIES += \
-    generated_kernel_headers \
+    qti_kernel_headers \
     libhardware_headers
 
 LOCAL_SHARED_LIBRARIES += \
